6d951fe57297e21557a78f1e1e48b3ab6380bcb6
doaso <doebutsu@gmail.com>
2026-01-04 08:10:34 +0000

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,80 +1,58 @@
 #!/bin/bash
-PARTITION="nvme0n1"
-PARTITION_SEPERATOR="p"
-parted /dev/${PARTITION} << EOF
+parted /dev/nvme0n1 << EOF
 mklabel gpt
-mkpart primary fat32 1MiB 513MiB
+mkpart primary fat32 1MiB 201MiB
 set 1 esp on
-name 1 EFI
-mkpart primary linux-swap 513MiB 16GiB
-name 2 swap
-mkpart primary ext4 16GiB 100%
-name 3 root
-quit
+mkpart primary ext4 201MiB 100%
 EOF
-mkfs.vfat -F 32 /dev/${PARTITION}${PARTITION_SEPERATOR}1
-mkswap /dev/${PARTITION}${PARTITION_SEPERATOR}2
-swapon /dev/${PARTITION}${PARTITION_SEPERATOR}2
-mkfs.ext4 /dev/${PARTITION}${PARTITION_SEPERATOR}3
-mkdir --parents /mnt/gentoo
-mount /dev/${PARTITION}${PARTITION_SEPERATOR}3 /mnt/gentoo
-mkdir --parents /mnt/gentoo/efi
-mount /dev/${PARTITION}${PARTITION_SEPERATOR}1 /mnt/gentoo/efi
-MIRROR="https://distfiles.gentoo.org"
-ARCHITECTURE="amd64"
-LATEST_STAGE=$(curl --silent ${MIRROR}/releases/${ARCHITECTURE}/autobuilds/current-stage3-${ARCHITECTURE}-musl-llvm/latest-stage3-${ARCHITECTURE}-musl-llvm.txt | grep --only-matching .*.tar.xz)
-wget --directory-prefix=/mnt/gentoo ${MIRROR}/releases/${ARCHITECTURE}/autobuilds/current-stage3-${ARCHITECTURE}-musl-llvm/${LATEST_STAGE}
-tar xpvf /mnt/gentoo/*.tar.xz --xattrs-include='*.*' --numeric-owner --directory=/mnt/gentoo
-rm --force /mnt/gentoo/*.tar.xz
-cp --recursive ./root/* /mnt/gentoo/
-cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
-ROOT_PASSWD="passwd"
-USER="doaso"
-USER_PASSWD="passwd"
-arch-chroot /mnt/gentoo /bin/bash << EOF
+mkfs.vfat -F 32 /dev/nvme0n1p1
+mkfs.ext4 /dev/nvme0n1p2
+mount /dev/nvme0n1p2 /mnt
+mkdir --parents /mnt/boot/efi
+mount /dev/nvme0n1p1 /mnt/boot/efi
+wget --directory-prefix=/mnt "https://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-musl-llvm/$(curl curl https://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-musl-llvm/latest-stage3-amd64-musl-llvm.txt | grep '.tar.xz' | awk '{print $1}')"
+tar xpvf /mnt/*.tar.xz --xattrs-include='*.*' --numeric-owner --directory=/mnt
+rm --force /mnt/*.tar.xz
+cp /etc/resolv.conf /mnt/etc/resolv.conf
+cp --recursive ./root/* /mnt/
+arch-chroot /mnt << EOF
+emerge-webrsync
 emerge --sync
-emerge sys-devel/gcc
-emerge sys-apps/musl-locales
-emerge sys-libs/timezone-data
-emerge sys-kernel/linux-firmware
-emerge sys-kernel/installkernel
-emerge sys-kernel/gentoo-kernel
-emerge sys-fs/genfstab
-emerge sys-boot/grub
-emerge app-admin/sysklogd
-emerge net-misc/chrony
-emerge net-misc/dhcpcd
-emerge net-wireless/iwd
-emerge media-video/pipewire
-emerge media-video/wireplumber
-emerge app-admin/doas
-emerge app-misc/neofetch
-emerge dev-vcs/git
-emerge app-editors/neovim
-emerge dev-util/tree-sitter-cli
-emerge gui-apps/wl-clipboard
-emerge dev-libs/wayland
-emerge gui-wm/sway
-emerge sys-auth/seatd
-emerge gui-apps/swaybg
-emerge gui-apps/waybar
-emerge gui-apps/grim
-emerge gui-apps/foot
-emerge dev-lang/rust
-emerge www-client/firefox
-emerge --verbose --update --deep --newuse @world
-emerge --deepclen
-echo "root:${ROOT_PASSWD}" | chpasswd
-useradd --create-home --groups users,wheel,pipewire --shell /bin/bash ${USER}
-echo "root:${USER_PASSWD}" | chpasswd
-rc-update add iwd default
-rc-update add dhcpcd default
-rc-update add sysklogd default
-rc-update add chronyd default
+emerge --quiet --update --newuse @world
 rc-update add elogind boot
-emerge sys-fs/genfstab
+emerge --quiet sys-devel/gcc
+emerge --quiet dev-lang/rust-bin
+emerge --quiet sys-kernel/linux-firmware
+emerge --quiet sys-kernel/gentoo-kernel-bin
+emerge --quiet sys-fs/genfstab
 genfstab -U / >> /etc/fstab
-emerge sys-boot/grub
-grub-install --efi-directory=/efi
+grub-install
 grub-mkconfig --output=/boot/grub/grub.cfg
+emerge --quiet sys-apps/musl-locales
+emerge --quiet sys-libs/timezone-data
+env-update && source /etc/profile
+emerge --quiet app-admin/sysklogd
+rc-update add sysklogd default
+emerge --quiet net-wireless/iwd
+rc-update add iwd default
+emerge --quiet net-misc/dhcpcd
+rc-update add dhcpcd default
+emerge --quiet app-admin/doas
+emerge --quiet media-video/pipewire
+useradd --create-home --groups users,wheel,pipewire --shell /bin/bash doaso
+echo 'doaso:password' | chpasswd
+echo 'root:password' | chpasswd
+emerge --quiet media-video/wireplumber
+emerge --quiet app-misc/neofetch
+emerge --quiet emerge dev-vcs/git
+emerge --quiet app-editors/neovim
+emerge --quiet dev-util/tree-sitter-cli
+emerge --quiet gui-apps/wl-clipboard
+emerge --quiet dev-libs/wayland
+emerge --quiet gui-wm/sway
+emerge --quiet gui-apps/swaybg
+emerge --quiet gui-apps/waybar
+emerge --quiet gui-apps/foot
+emerge --quiet gui-apps/grim
+emerge --quiet www-client/firefox
 EOF
